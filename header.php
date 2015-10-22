4075e4ff007d966fd7cdc4581184c118fd165c96
demkacode <demkacode@gmail.com>
2015-10-22 14:14:55 +0300

    php include

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sedona — Index</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=PT+Sans:400,700&subset=latin,cyrillic" type='text/css'>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+
+        <div class="container">
